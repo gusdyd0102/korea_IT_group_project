fbc192382b85fe3cf4ae6ed3fc336469f332d394
Fk1128 <ohyhsf@naver.com>
2024-11-27 15:33:21 +0900

    login

---
+++ spring/work/map/src/main/java/project/map/dto/ResponseDTO.java
@@ -0,0 +1,22 @@
+package project.map.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO<T> {
+
+	
+	private String error ;
+	private List<T> data ;
+	private T value ;
+	
+	
+}
---
+++ spring/work/map/src/main/java/project/map/entity/UserEntity.java
@@ -0,0 +1,35 @@
+package project.map.entity;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idx; //식별자 
+	private String userId; // 회원아이디
+	private String password; // 비밀번호
+	private String userName ; // 이름
+	private String email; // 이메일
+	private String address; // 주소 
+	private String profilePhoto; // 프로필사진
+	private Date birthDate; // 생년월일
+	@CreationTimestamp
+	private Date signupDate; // 회원가입 날짜 
+}
